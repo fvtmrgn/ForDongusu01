bb16e6512597c569f5c487b5bd46f2d7836c22f4
Fatımatüzzehra Ergen <88490838+fvtmrgn@users.noreply.github.com>
2024-01-02 19:09:49 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForDongusu01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int t = 0, b = 0;
+
+            for (; ; )
+            {
+                Console.Write("Sayı giriniz:");
+                int a = Convert.ToInt32(Console.ReadLine());
+
+                if (a == 83)
+                {
+                    Console.WriteLine("toplam:" + t);
+                    break;
+
+                }
+
+                b = t + a;
+                t = b;
+
+
+
+            }
+
+        }
+    }
+}
